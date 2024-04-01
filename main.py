5c50a49e4cc188ff1d98a34a833b889fd01e9828
MagyoDev <alandelimasilva51@gmail.com>
2024-04-01 12:38:33 -0300

    Introduction

---
+++ main.py
@@ -0,0 +1,9 @@
+# Name and age
+name = input("What's your name? ")
+age = int(input("What's your age? "))
+
+print(name)
+print(age,'\n')
+print(name, age,'\n')
+print("Your name is {}, age is {}".format(name,age))
+
